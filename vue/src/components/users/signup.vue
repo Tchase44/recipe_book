9f48eeb56719ee75e51c7d21ff7090860066dc8e
tchase44 <tchase4544@gmail.com>
2020-05-19 13:57:07 -0400

    styles and signup prep

--- vue/src/components/users/signup.vue
+++ vue/src/components/users/signup.vue
@@ -1,22 +1,52 @@
 <template>
   <div>
-    <form action="">
+    <form @submit="requestAccount" action="">
       <label for="email">email</label><br>
-      <input type="text" name="email" class=""><br>
+      <input type="email" name="email" class="" required v-model="pUser.email"><br>
       <label for="username">username</label><br>
-      <input type="text" name="username" class=""><br>
+      <input type="text" name="username" class="" v-model="pUser.username"><br>
       <label for="password">password</label><br>
-      <input type="text" name="password" class=""><br>
+      <input type="password" name="password" class="" required v-model="pUser.password"><br>
       <label for="password_confirmation">password_confirmation</label><br>
-      <input type="text" name="password_confirmation" class=""><br>
+      <input type="password" name="password_confirmation" class="" required v-model="pUser.password_confirmation"><br>
       <button type="submit">Sign Up</button>
     </form>
   </div>
 </template>
 
 <script>
+import userApi from '../../services/userApi'
 export default {
-  name: "SignUp"
+  name: "SignUp",
+  data(){
+    return {
+      pUser:{
+        email: "",
+        username: "",
+        password: "",
+        password_confirmation:""
+      },
+      vaild: ""
+    }
+  },
+  methods:{
+    passCheck(s1,s2){
+      return (typeof(s1) === "string" && typeof(s2) === "string" && s1 === s2 )
+    },
+    emailCheck(email){
+      // super basic check
+      return (email.indexOf("@") !== -1 && email.indexOf(".") !== -1 && typeof(email) === "string" )
+    }
+  },
+  async requestAccount(e){
+    e.preventDefault();
+    if(passCheck(this.pUser.pasword,this.pUser.password_confirmation) && emailCheck(this.pUser.email)){
+      // ToDo: API activation email
+      let success = await userApi.signUp(this.pUser)
+    } else {
+      this.vaild = "Please check your Email or Password"
+    }
+  }
 }
 </script>
 
